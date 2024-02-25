5725bde77444cf5c3eda4fd952126e756d644ca9
asadbekdev07 <futureprotrader1007@gmail.com>
2024-02-25 17:11:41 +0500

    Add modal js setTimeout and modal close function

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const elModal = document.querySelector(".modal");
+const elModalClose = elModal.querySelector(".js-modal-close");
+
+
+if (elModalClose) {
+  elModalClose.addEventListener("click", function () {
+    elModal.classList.remove("modal__open")
+  });
+}
+
+setTimeout(function () {
+  elModal.classList.add("modal__open");
+}, 30000)
